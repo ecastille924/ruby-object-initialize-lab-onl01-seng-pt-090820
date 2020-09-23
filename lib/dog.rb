5cb04d8e2fa6d0ab3a4ce653cbb0a7e5bed97da1
Erick Castille <ecastille924@gmail.com>
2020-09-23 22:33:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,4 +2,20 @@ class Dog
   def initialize(name, breed)
     @name, @breed = name, breed 
   end
+  
+  def name= name
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed= breed
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
 end
